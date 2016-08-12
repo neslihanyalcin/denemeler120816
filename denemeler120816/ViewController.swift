f32f010b7534a5bae5ceb15ef2ec098eb6a96643
Adnan-iMac <Adnan-iMac@adnan-imac-imac.mltlocal.com.tr>
2016-08-12 10:49:56 +0300

    Initial Commit

---
+++ denemeler120816/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  denemeler120816
+//
+//  Created by Adnan-iMac on 12.08.2016.
+//  Copyright © 2016 Neslihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
